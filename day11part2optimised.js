f99586b3a1fa72b02c87599fd3f6556378116d39
Alexander Ogier <aogier@TradeMe.local>
2018-12-12 09:13:56 +1300

    further optimise by not accessing dictionary using string

--- day11part2optimised.js
+++ day11part2optimised.js
@@ -4,6 +4,14 @@ var sumAreaTable = {};
 
 var startTime = new Date();
 
+function insertPower(x, y, power) {
+	sumAreaTable[x + y * 300] = power;
+}
+
+function getPower(x, y) {
+	return sumAreaTable[x + y * 300];
+}
+
 for(var x = 1; x <= 300; x++) {
 	for(var y = 1; y <= 300; y++) {
 		var rankId = x + 10;
@@ -19,16 +27,16 @@ for(var x = 1; x <= 300; x++) {
 		var sumArea = powerLevel;
 		
 		if (x > 1) {
-			sumArea += sumAreaTable[(x-1) + ',' + y];
+			sumArea += getPower(x-1, y);
 		}
 		if (y > 1) {
-			sumArea += sumAreaTable[x + ',' + (y-1)];
+			sumArea += getPower(x, y-1);
 		}
 		if (x > 1 && y > 1) {
-			sumArea -= sumAreaTable[(x-1) + ',' + (y-1)];
+			sumArea -= getPower(x-1, y-1);
 		}
 		
-		sumAreaTable[x+','+y] = sumArea;
+		insertPower(x, y, sumArea);
 	}
 }
 
@@ -41,16 +49,16 @@ for(var x = 1; x < 298; x++) {
 	for(var y = 1; y < 298; y++) {
 		
 		for(var squareSize = 1; squareSize <= 300 - Math.max(x, y); squareSize++) {
-			var score = sumAreaTable[(x+squareSize-1)+','+(y+squareSize-1)];
+			var score = getPower(x+squareSize-1, y+squareSize-1);
 			
 			if (x > 1) {
-				score -= sumAreaTable[(x-1) + ',' + (y+squareSize-1)];
+				score -= getPower(x-1, y+squareSize-1);
 			}
 			if (y > 1) {
-				score -= sumAreaTable[(x+squareSize-1) + ',' + (y-1)];
+				score -= getPower(x+squareSize-1, y-1);
 			}
 			if (x > 1 && y > 1) {
-				score += sumAreaTable[(x-1) + ',' + (y-1)];
+				score += getPower(x-1, y-1);
 			}
 						
 			if (score > bestScore) {
